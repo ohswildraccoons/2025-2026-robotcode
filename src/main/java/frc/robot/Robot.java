95e859109a3ff3f41c0ea75004626514aba1a861
JohnGaby <jgaby@gabysoft.com>
2026-01-17 16:58:25 -0800

    Got the intake running as well ;3

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,15 @@
 
 package frc.robot;
 
+import com.revrobotics.spark.SparkFlex;
+import com.thethriftybot.ThriftyNova.MotorType;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
 
 
 
@@ -21,6 +27,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
+  private final SparkFlex mFlex;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -30,6 +37,11 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    mFlex = new SparkFlex(11, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
+    mFlex.set(100);
+
+
+    
   }
 
   /**
@@ -46,6 +58,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
---
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -0,0 +1,122 @@
+package frc.robot.subsystems;
+
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Feet;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.Seconds;
+
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.SmartMechanism;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.local.SparkWrapper;
+
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+
+public class TurretSubsystem extends SubsystemBase  {
+        
+    private SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
+    .withControlMode(ControlMode.CLOSED_LOOP)
+
+    // Feedback Constants (PID Constants)
+    .withClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+    .withSimClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+
+    // Feedforward Constants
+    .withFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+    .withSimFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+
+    // Telemetry name and verbosity level
+    .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
+
+    // Gearing from the motor rotor to final shaft.
+    // In this example GearBox.fromReductionStages(3,4) is the same as GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to your motor.
+    // You could also use .withGearing(12) which does the same thing.
+    .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+
+    // Motor properties to prevent over currenting.
+    .withMotorInverted(false)
+    .withIdleMode(MotorMode.COAST)
+    .withStatorCurrentLimit(Amps.of(40)); 
+
+    
+    // Vendor motor controller object
+    private SparkMax spark = new SparkMax(4, MotorType.kBrushless);
+
+    // Create our SmartMotorController from our Spark and config with the NEO.
+    private SmartMotorController sparkSmartMotorController = new SparkWrapper(spark, DCMotor.getNEO(1), smcConfig);
+
+  
+    private final FlyWheelConfig shooterConfig = new FlyWheelConfig(sparkSmartMotorController)
+    // Diameter of the flywheel.
+    .withDiameter(Inches.of(4))
+    // Mass of the flywheel.
+    .withMass(Pounds.of(1))
+    // Maximum speed of the shooter.
+    
+    // .withUpperSoftLimit(RPM.of(1000))
+    // Telemetry name and verbosity for the arm.
+    .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH);
+
+    // Shooter Mechanism
+    private FlyWheel shooter = new FlyWheel(shooterConfig);
+
+
+  /** Creates a new ExampleSubsystem. */
+  public TurretSubsystem() {}
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    shooter.updateTelemetry();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+    shooter.simIterate();
+  }
+}
+    
